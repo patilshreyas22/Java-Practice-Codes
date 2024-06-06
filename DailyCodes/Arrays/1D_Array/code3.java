3c3f689a4b0c727eccbe469d04775d5fc0e27d22
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-06-06 21:06:55 +0300

    array basic

---
+++ DailyCodes/Arrays/1D_Array/code3.java
@@ -0,0 +1,45 @@
+class playerjer{
+
+
+	public static void main(String args []){
+	
+	
+		int jerno1 = 10;
+	
+		int jerno2 = 45;
+	
+		int jerno3 = 18;
+	
+		int jerno4 = 7;
+	
+		int jerno5 = 24;
+	
+		int jerno6 = 11;
+
+
+
+
+		System.out.println("jerno1 : " + jerno1);
+	
+		System.out.println("jerno2 : " + jerno2);
+	
+		System.out.println("jerno3  : " + jerno3);
+	
+		System.out.println("jerno4 : " + jerno4);
+	
+		System.out.println("jerno5 : " +  jerno5);
+	
+		System.out.println("jerno6 : " +  jerno6);
+	
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+}
