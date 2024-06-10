7248c52fa356b6d30968a66ca3b4d95405bf72d8
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-06-10 12:40:03 +0300

    1d_array

---
+++ DailyCodes/Arrays/1D_Array/code16.java
@@ -0,0 +1,25 @@
+class code16{
+
+	public static void main(String args []){
+	
+	
+		int [] empID = new int[4]{10,20,30,40};
+
+		 
+		for(int i = 0 ; i<4 ; i++){
+		
+			System.out.println(empID[i]);
+		
+		
+		}
+
+
+	
+	
+	
+	}
+
+
+
+
+}
---
+++ DailyCodes/Arrays/1D_Array/code17.java
@@ -0,0 +1,39 @@
+class code17{
+
+	public static void main(String args []){
+	
+	
+		int [] empID = new int[]{10,20,30,40};
+
+		 
+		for(int i = 0 ; i<4 ; i++){
+		
+			System.out.println(empID[i]);
+		
+		
+		}
+
+		empID[0] = 110 ;
+
+		empID[1] = 120 ;
+
+		empID[2] = 130 ;
+	
+		empID[3] = 140 ;
+	
+	
+		for(int i = 0 ; i<4 ; i++){
+		
+			System.out.println(empID[i]);
+		
+	
+		}
+		
+		}
+
+
+
+
+
+
+}
