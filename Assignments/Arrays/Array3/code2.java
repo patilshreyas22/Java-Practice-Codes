f0eb364759cf77dd2242b747b5d6dffa1b8589e8
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-06-12 15:41:40 +0300

    array3

---
+++ Assignments/Arrays/Array3/code2.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class code2{
+
+
+	public static void main(String args []){
+	
+	
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the size of the Array : ");
+		
+		int size = sc.nextInt();
+
+		int arr [] = new int [size];
+		
+		for(int i = 0 ; i<arr.length ; i++){
+		
+			arr[i] = sc.nextInt();
+		
+		
+		}
+
+		System.out.println("Enter the number to be searched : ");
+
+		int num = sc.nextInt();
+
+		for(int i = 0 ; i<arr.length ; i++){
+		
+		
+		
+
+		
+		
+		}
+	
+	
+	
+	}
+
+
+
+
+}
