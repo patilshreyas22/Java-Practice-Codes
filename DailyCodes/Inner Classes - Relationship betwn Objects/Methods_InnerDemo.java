8968685cfcad6f9e14c077f525cc8f34cb7cea45
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-06-12 10:59:11 +0300

    static/instance methods in the inner class

--- DailyCodes/Inner Classes - Relationship betwn Objects/Methods_InnerDemo.java
+++ DailyCodes/Inner Classes - Relationship betwn Objects/Methods_InnerDemo.java
@@ -12,30 +12,30 @@ class Outer{
 
 	class Inner{
 	
-		//int x = 40;
+		int x = 40;
 	
 	Inner(){
-		 int x = 40;
+		// int x = 40;
 		System.out.println("In Inner Constructor");
 		System.out.println(Outer.this.x);
 		
 		System.out.println(y);
+		fun();
 		
 	}
+}
 
+	void fun(){
 	
-	
-	
-	
-	
+		System.out.println("In Fun Method");
 	
 	}
 
 	public static void main(String args []){
 	
-	
-		Inner obj = new Outer().new Inner();
-	
+		Outer obj1 = new Outer();
+		Inner obj = obj1.new Inner();
+	     //   obj1.fun();
 	
 	}
 
