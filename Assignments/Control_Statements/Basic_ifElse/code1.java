7c8c6dcdcc1d7896e7883c3194b8fadaa4c72acc
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-06-14 19:44:21 +0300

    basicifelse

---
+++ Assignments/Control_Statements/Basic_ifElse/code1.java
@@ -0,0 +1,22 @@
+class code1{
+	
+	public static void main (String args []){
+	
+		int num = 5;
+		int num1 = -9;
+
+		if(num>0){
+		
+			System.out.println(num + " is a positive number");
+		
+		}
+		if(num1<0){
+		
+			System.out.println(num1 + " is a negative number");
+		}
+	
+	
+	}
+
+
+}
