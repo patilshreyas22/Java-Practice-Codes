e6067a6ca33d1fe162fcf5e18ea46ea5464ef2aa
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-06-17 11:37:05 +0300

    array4

---
+++ Assignments/Arrays/Array4/code2.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class code2{
+
+
+
+	public static void main(String args []){
+	
+	
+		Scanner sc = new Scanner(System.in);
+
+		int size = sc.nextInt();
+
+		int arr [] = new int [size];
+
+		for(int i = 0 ; i<arr.length ; i++){
+		
+			arr[i] = sc.nextInt();
+		
+		}
+
+		int min = arr[0];
+		int max = arr[0];
+
+		for(int i = 0 ; i<arr.length ; i++){
+		
+			if(arr[i] < min){
+			
+				min = arr[i];
+			}
+			if(arr[i] > max){
+			
+				max = arr[i];
+			}
+		
+		
+		}
+
+		System.out.println("Difference " + (max - min));
+	
+	
+	}
+
+
+
+
+}
