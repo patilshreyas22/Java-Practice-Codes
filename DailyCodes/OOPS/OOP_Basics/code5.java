f6aaa967b5321ba18ddb4722e663dd25a92cae6b
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-06-30 11:48:12 +0300

    oop basics

---
+++ DailyCodes/OOPS/OOP_Basics/code5.java
@@ -0,0 +1,12 @@
+class code5{
+
+	static int x = 10;
+
+	public static void main (String args []){
+	
+		System.out.println(x);
+	
+	}
+
+
+}
---
+++ DailyCodes/OOPS/OOP_Basics/code6.java
@@ -0,0 +1,21 @@
+class code6{
+
+	 int x = 10;
+
+	public static void main (String args []){
+	
+		code6 obj = new code6();
+
+		
+		obj.run();
+
+	
+	}
+
+	void run(){
+	
+		System.out.println(x);
+	}
+
+
+}
