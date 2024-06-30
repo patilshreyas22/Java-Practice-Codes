17ef7b10e2ff1155304ee85d29ba288567023e61
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-06-30 11:42:49 +0300

    oop basics

---
+++ DailyCodes/OOPS/OOP_Basics/code4.java
@@ -0,0 +1,13 @@
+class code4{
+
+	int x = 1 ;
+	// non static variable x cannot be references from a static context which the main method
+
+	public static void main(String args []){
+	
+		System.out.println(x);
+	
+	}
+
+
+}
