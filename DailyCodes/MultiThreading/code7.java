a8e72bcd6a55468a1c5cdc237d0309dfc856151b
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-07-11 09:43:57 +0300

    Thread Methods

---
+++ DailyCodes/MultiThreading/code7.java
@@ -0,0 +1,40 @@
+class demo extends Thread{
+
+	public demo(String name){
+	
+	
+			super(name);
+	
+	
+	}
+
+
+}
+class ThreadTest {
+
+	public static void main(String args []){
+	
+		demo obj = new demo("demo thread");
+
+		System.out.println("Demo Thread ID : " +  obj.getId());
+
+		System.out.println(obj.getName());
+
+		System.out.println("Main Thread ID : " + Thread.currentThread().getId());	
+
+		System.out.println(Thread.currentThread().getName());
+
+		obj.start(); // if the thread isnt started yet , the below SOP line will show "false" that it is not alive
+
+		System.out.println("Demo " + obj.isAlive());
+
+		System.out.println("Main " + Thread.currentThread().isAlive());
+
+		System.out.println("Demo Priority " + obj.getPriority());
+
+		System.out.println("Demo State " + obj.getState());
+	
+	}
+
+
+}
