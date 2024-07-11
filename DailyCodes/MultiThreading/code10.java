09354971ab1bb038339ed0ab781ca786ee18d9a1
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-07-11 10:21:24 +0300

    priority of thread and interrupting a thread

---
+++ DailyCodes/MultiThreading/code10.java
@@ -0,0 +1,48 @@
+class demo extends Thread{
+
+	public demo(String name){
+	
+		super(name);
+	
+	}
+	
+	
+	
+	public void run(){
+	
+	
+		int count = 1;
+
+		while(true){
+		
+			System.out.println(count++);
+			try{
+			Thread.sleep(1000);
+			}catch(InterruptedException ie){
+				
+				System.out.println(ie);
+			}
+		
+		}
+	
+	
+	}
+
+}
+
+class client{
+
+	public static void main(String args []){
+
+	demo obj = new demo("Demo Thread"); // when extending a Thread Class , you have to call the super method of the thread class to use the name method of that class to set name of the newly created thread
+	
+	obj.start();
+
+	obj.interrupt();
+	
+	}
+
+
+
+
+}
