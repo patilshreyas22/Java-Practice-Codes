740e9b2b0c3beea110e848c43c1a9ac9ec6aee31
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-07-13 18:44:03 +0300

    Flow of the JavaFX Application , main-init-start-main

--- JavaFX/demo/src/main/java/com/example/Main.java
+++ JavaFX/demo/src/main/java/com/example/Main.java
@@ -16,25 +16,19 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void init()throws Exception{  //this inti method is overriden from the Application class , it has a separate thread from the main thread , this inti method runs before the launch(args) thread launches the application 
-                                        // so basically we can do connections in the init method , before the application starts 
+    public void init() throws Exception { // this inti method is overriden from the Application class , it has a
+                                          // separate thread from the main thread , this inti method runs before the
+                                          // launch(args) thread launches the application
+                                          // so basically we can do connections in the init method , before the
+                                          // application starts
         System.out.println("Before the App Starts");
+        System.out.println("Thread Name : " + Thread.currentThread().getName());
         Thread.sleep(5000);
     }
-
-
-
-
 
     @Override
 
     public void start(Stage stage) throws Exception {
-
-        
-
-        
-
-
 
         VBox vb = new VBox();
 
@@ -44,13 +38,14 @@ public class Main extends Application {
         Button b = new Button("Hello!");
 
         Label lb = new Label("");
-        
+
         b.setOnAction(e -> {
             lb.setText("Welcome to JavaFX Application");
         });
 
-        vb.getChildren().addAll(b  , lb);
-        
+        vb.getChildren().addAll(b, lb);
+
+        System.out.println("Thread Name : " + Thread.currentThread().getName());
         Scene sc = new Scene(vb, 350, 300);
         stage.setScene(sc);
         stage.setTitle("JavaFX Application");
@@ -59,9 +54,9 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        
-        launch(args);
 
-        System.out.println("After the App is Started");
+        System.out.println("Thread Name : " + Thread.currentThread().getName());
+        launch(args);
+        System.out.println("After the App is Closed");
     }
 }
