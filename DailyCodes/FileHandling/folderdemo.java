feba14ed88c7daeaffe487b9dd95ed8de4c1cd6d
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-07-22 08:21:04 +0300

    Creating a Folder

---
+++ DailyCodes/FileHandling/folderdemo.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+class Demo{
+
+	public static void main(String args []){
+	
+	
+		File dirObj = new File("FolderDemo");
+
+		dirObj.mkdir();  // this tells us that java uses the UNIX FILE SYSTEM 
+		
+		// also the above written line doesnt throw any exception
+
+	
+	
+	
+	
+	}
+
+
+
+
+
+}
