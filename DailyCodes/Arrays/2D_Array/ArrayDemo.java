dd4673d2a0267528baf18efa5714ee0e2ca9e62d
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-08-04 21:07:37 +0300

    Revisiting 2D-Arrays

---
+++ DailyCodes/Arrays/2D_Array/ArrayDemo.java
@@ -0,0 +1,8 @@
+class ArrayDemo{
+	
+	public static void main(String args []){
+	
+		int arr [] = new int[5]{};  // You cannot create a array by giving both Dimension and Initialization , only one at a time is valid
+	}
+
+}
