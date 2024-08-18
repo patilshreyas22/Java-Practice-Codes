cd0c7ca9e05bee40dd9751a6e089f4a754cb566d
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-08-18 21:16:29 +0300

    PriorityBlockingQueue parameter

--- DailyCodes/Collection/QUEUE/BlockingQue3.java
+++ DailyCodes/Collection/QUEUE/BlockingQue3.java
@@ -7,8 +7,9 @@ class BQD{
 	public static void main(String args [])throws InterruptedException{
 	
 		
-		BlockingQueue bq = new PriorityBlockingQueue(5); //the integer we are passing here in the PriorityBlockingQueue is not the size of the queue but it is related
-								 //to the priority of the storing of elements
+		BlockingQueue bq = new PriorityBlockingQueue(5); //the integer we are passing here in the PriorityBlockingQueue is not the fixed size of the queue but is the initial
+								 //capacity of the internal array used by the PriorityBlockingQueue.However since the queue is unbounded , this capacity
+								 //will dynamically increase as more elements are added
 	
 		bq.put(10);
 		bq.put(20);
