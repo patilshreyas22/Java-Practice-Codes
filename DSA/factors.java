3e04316f94735f38bb19227e057568884ecb1c1e
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-08-19 11:10:49 +0300

    factors of numbers using function

---
+++ DSA/factors.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class factor{
+		
+	static int count = 0;
+	
+	static int check(int number){
+		
+		if(number>0){
+		for(int i = 1 ; i<=number ; i++){
+			
+			if(number%i==0){
+				
+				count++;
+			
+			}
+		
+		}
+	
+	
+	
+	}else{
+		
+		System.out.println("Invalid Number");
+	}
+
+	return number;
+	
+}
+	
+	
+	
+	public static void main(String args []){
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a number :");
+
+		int number = sc.nextInt();
+
+		check(number);
+
+		System.out.println("Total factors of " + number + " : " + count);
+	
+	}
+
+}
