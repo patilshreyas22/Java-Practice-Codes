889fdc7a8ca2ee87cbf8c11cd61252b206f6123b
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-09-07 13:38:05 +0300

    Difference Count Brute Force Approach

---
+++ DSA/DifferenceCount.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class client{
+	
+	static int operations(int start , int end){
+		
+		int count = 2;
+		for(int i = start+1 ; i<end ; i++){
+		
+			count++;
+
+		
+		}
+	
+		return count;
+	}
+
+	public static void main(String args []){
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the starting and the ending number : ");
+
+		int start = sc.nextInt();
+
+		int end = sc.nextInt();
+
+		System.out.println(operations(start , end));
+	
+	}
+
+
+}
