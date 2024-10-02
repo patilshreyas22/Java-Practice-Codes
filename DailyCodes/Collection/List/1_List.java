c8658998c6f4671ac1b7d728621979a5ed997bdf
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-10-02 13:14:29 +0300

    list demo

--- DailyCodes/Collection/List/1_List.java
+++ DailyCodes/Collection/List/1_List.java
@@ -4,8 +4,14 @@ class ArrayListDemo{
 	
 	public static void main(String args []){
 		
-		List list = new ArrayList(); 
+		List<Integer> list = new ArrayList<Integer>();
+
+		list.add(10);
+		list.add(20);
+		list.add(30);
+
+	System.out.println(list);	
 	
-	}
+	}  
 
 }
