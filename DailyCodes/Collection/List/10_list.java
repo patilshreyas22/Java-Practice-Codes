f9fd2e2391fd31b414c36992f13c0b71c82b60e0
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-10-04 12:42:35 +0300

    all the methods of the list with explanations

--- DailyCodes/Collection/List/10_list.java
+++ DailyCodes/Collection/List/10_list.java
@@ -29,9 +29,28 @@ class ListDemo{
 
 		System.out.println(list.get(5)); //N
 		
-		System.out.println(list.contains(42.53)); //false
+		System.out.println(list.contains(42.53)); //false 
+		
+		list.add(3 ,5);
 
-	
+		System.out.println(list); //whole list
+		
+		//there are two types of 'remove' method , 1)remove(int index) and 2)remove(Object o)
+		//
+		//the remove(int index) returns the element which was removed 
+		//the remove(object o) returns a boolean value(true/false)
+		//the removem (object o) removes the first occurrence of the specified element from the list , if present returns TRUE , otherwise returns FALSE if the element was not found
+		
+		System.out.println(list.remove(5)); //b
+
+		System.out.println(list); //whole list
+
+		System.out.println(list.remove("Shreyas")); //true
+
+		System.out.println(list); //whole list
+
+		System.out.println(list.size()); //6
+
 		list.clear();
 
 		System.out.println(list.isEmpty()); //true
