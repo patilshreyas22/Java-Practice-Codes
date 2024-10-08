385d8377624bc238f77f1c653b740be232d09205
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-10-08 11:01:07 +0300

    LinkedList ForEach Demo

---
+++ DailyCodes/Collection/Enumeration_Iterator/5_ForEach.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class ForEachDemo{
+	
+	public static void main(String args []){
+	
+	
+		
+		LinkedList ll = new LinkedList();
+
+		ll.add("Shreyas");
+		ll.add(22);
+		ll.add("Shrishti");
+		ll.add(30);
+		ll.add("Aditya");
+		ll.add(24);
+		ll.add("Rekha");
+		ll.add(10);
+		ll.add("Anil");
+		ll.add(25);
+		
+		for(Object x : ll){
+			
+			System.out.println(x);
+		
+		}
+	
+	}
+
+}
