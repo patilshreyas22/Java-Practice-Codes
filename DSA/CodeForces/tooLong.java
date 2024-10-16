ee100f47c462503118e456af3f513f9bd89ecc6b
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-10-16 21:45:54 +0300

    tooLong String

--- DSA/CodeForces/tooLong.java
+++ DSA/CodeForces/tooLong.java
@@ -7,21 +7,43 @@ public class tooLong{
 
 		public static void main(String args []){
 		
+
+		
 		Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
+		int count = sc.nextInt();
+		sc.nextLine(); //consumes the nextLine which wasnt consumed by the sc.nextInt call
 
-		StringBuilder arr [] = new StringBuilder[n];
+		StringBuilder [] SBarray = new StringBuilder[count];
 
-		for(int i = 0 ; i<n ; i++){
+		for(int i = 0 ; i<count ; i++ ){
+			
+			String word = sc.nextLine();
+
+			StringBuilder sbword = new StringBuilder(word);
+
+			if(sbword.length()>10){
+				
+				sbword.replace(1 , sbword.length()-1 , sbword.length()-2+"");
+
+				SBarray[i]=sbword;
+			
+			}else{
+				
+				SBarray[i]=sbword;
+			
+			}
 		
-			arr[i] = sc.nextLine();
 		
 		}
-		
-		
 
+		for(int i = 0 ; i<count ; i++ ){
 			
+			System.out.println(SBarray[i]);
+		
+		}
+
+					
 	}
 
 
