1d08d5e5a2107f74ab0f3f1c4380bfabe2f0194e
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-10-19 10:22:24 +0300

    Arrays Lec 6

---
+++ DSA/ArrayN.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class demo{
+	
+	public static void main(String args []){
+	
+		int N = 6;
+		int [] arr = new int[]{5 , 6 , 2 , 3 , 1 , 9};
+
+		for(int i = 0 ; i<arr.length ; i++){
+			
+			System.out.print(arr[i] + " ");
+		
+		}
+		System.out.println();
+
+		//Time Complexity : O(N)
+		//Space Complexity : O(1)
+	
+	}
+
+}
