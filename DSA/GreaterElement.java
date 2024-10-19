da37a42d75df65bd9065dd88ab44dc44892b72a2
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-10-19 15:43:00 +0300

    GreaterElement

---
+++ DSA/GreaterElement.java
@@ -0,0 +1,35 @@
+class demo{
+	
+	public static void main(String args []){
+		
+		int N = 10;
+		int arr [] = new int [] {2 , 3 , 2 , 12 , 8, 12 , 8 , 1 , 7 , 8};
+		
+		int max = arr[0];
+
+		int count = 0;
+
+		for(int i = 1 ; i < arr.length ; i++){
+			
+			if(arr[i] > max){
+			
+				max = arr[i];
+			}
+			
+		}
+
+		for(int i = 0 ; i < arr.length ; i++){
+		
+			if(arr[i] == max){
+				
+				count++;
+			}
+
+		}
+
+		System.out.println(arr.length-count);
+
+
+		
+		}
+}
