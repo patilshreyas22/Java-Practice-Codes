8baf3e97cfd3f562af6a11c004e8e1f11c323deb
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-10-26 09:39:50 +0300

    BOyGirl

---
+++ DSA/CodeForces/BoyGirl.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class BoyGirl{
+
+	
+
+	public static void main(String args []){
+	
+		
+		Scanner sc = new Scanner(System.in);
+
+		String username = sc.nextLine();
+
+		Set<Character> uniquewords = new LinkedHashSet<Character>();
+		
+
+		for(int i = 0 ; i<username.length() ; i++){
+				
+			uniquewords.add(username.charAt(i));
+
+		}
+
+		if(uniquewords.size()%2==0){
+			
+			System.out.println("CHAT WITH HER !");
+		
+		}else{
+			
+			System.out.println("IGNORE HIM !");
+		
+		}
+	
+	}
+
+
+
+}
