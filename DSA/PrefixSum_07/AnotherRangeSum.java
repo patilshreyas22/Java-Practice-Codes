1b599b84fed86e496d9419c783eebad875aaa1ad
patilshreyas22 <shreyaspatil.scoe.comp@gmail.com>
2024-10-27 21:26:09 +0300

    AnotherSum

--- DSA/PrefixSum_07/AnotherRangeSum.java
+++ DSA/PrefixSum_07/AnotherRangeSum.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+//Total TC of the Program : O(Q) * O(N) = O(Q*N)
+
 class demo{
 	
 	public static void main(String args []){
@@ -14,8 +16,9 @@ class demo{
 		int queries = 3;
 
 	
+			for(int j = 1 ; j<= queries ; j++){ //just realised that we cannot initiate nested for loops with same variables that is (int i) etc etc
 			
-			for(int j = 1 ; j<= queries ; j++){  //just realised that we cannot initiate nested for loops with same variables that is (int i) etc etc
+			// outer for loop TC : O(Q)
 
 			int start = sc.nextInt();
 
@@ -30,6 +33,7 @@ class demo{
 				
 				for(int i = start ; i<= end ; i++){
 				
+				// inner for loop TC : O(N)
 					
 					sum += arr[i];
 				
